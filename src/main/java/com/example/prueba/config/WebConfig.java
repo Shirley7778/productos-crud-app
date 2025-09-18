b285b83db12d39ec7a25288995919bb9dc2a379b
Shirley7778 <karenshirley777@gmail.com>
2025-09-18 06:22:44 -0500

    listapro

--- src/main/java/com/example/prueba/config/WebConfig.java
+++ src/main/java/com/example/prueba/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class WebConfig {
     @Bean
@@ -16,9 +15,9 @@ public class WebConfig {
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("*")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://euphonious-fox-ce60a3.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(false); // Importante: false cuando usas *
             }
         };
     }
